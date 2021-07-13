557a1bed4b6031fa6047de80bd42831c67bbed6e
TatjanaSokolova <tsvm@inbox.lv>
2021-07-13 16:33:59 +0300

    months added

--- src/task_3.java
+++ src/task_3.java
@@ -25,6 +25,39 @@ public class task_3 {
                 case 2:
                     System.out.println("It is February");
                     break;
+                case 3:
+                    System.out.println("It is March");
+                    break;
+                case 4:
+                    System.out.println("It is April");
+                    break;
+                case 5:
+                    System.out.println("It is May");
+                    break;
+                case 6:
+                    System.out.println("It is June");
+                    break;
+                case 7:
+                    System.out.println("It is July");
+                    break;
+                case 8:
+                    System.out.println("It is August");
+                    break;
+                case 9:
+                    System.out.println("It is September");
+                    break;
+                case 10:
+                    System.out.println("It is Oktober");
+                    break;
+                case 11:
+                    System.out.println("It is November");
+                    break;
+                case 12:
+                    System.out.println("It is December");
+                    break;
+
+
+
             }
         }
     }
